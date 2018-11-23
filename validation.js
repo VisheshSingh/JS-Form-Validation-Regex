0c7f3ec10428e9a75799d74fc84df62535200b3e
Vishesh <visheshsingh16@gmail.com>
2018-11-22 21:45:22 -0500

    Phone number validation

--- validation.js
+++ validation.js
@@ -39,4 +39,13 @@ function validateEmail() {
   }
 }
 
-function validatePhone() {}
+function validatePhone() {
+  const phone = document.getElementById("phone");
+  const re = /^\(?\d{3}\)?[-\.\s]?\d{3}[-\s\.]?\d{4}$/;
+
+  if (re.test(phone.value)) {
+    phone.classList.remove("is-invalid");
+  } else {
+    phone.classList.add("is-invalid");
+  }
+}
